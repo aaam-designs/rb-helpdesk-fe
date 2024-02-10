d4bc8279160a263a0d4dd22c7337f234734daf4d
aaam-designs <aaamdesigns@gmail.com>
2024-02-10 11:05:08 +0530

    changes

--- src/pages/FbPages/index.jsx
+++ src/pages/FbPages/index.jsx
@@ -26,23 +26,25 @@ const FbPages = () => {
         <div className="form-container">
           <h4>Select a page</h4>
           <br />
-          {pages.map((page) => {
-            return (
-              <Link
-                style={{
-                  textDecoration: "none",
-                  color: "#fff",
-                  background: "#183b70",
-                  padding: "10px",
-                  borderRadius: "5px",
-                  margin: "10px",
-                }}
-                to={`/messenger?page=${page.id}`}
-              >
-                {page.name}
-              </Link>
-            );
-          })}
+          {pages &&
+            pages.lenth > 0 &&
+            pages.map((page) => {
+              return (
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "#fff",
+                    background: "#183b70",
+                    padding: "10px",
+                    borderRadius: "5px",
+                    margin: "10px",
+                  }}
+                  to={`/messenger?page=${page.id}`}
+                >
+                  {page.name}
+                </Link>
+              );
+            })}
         </div>
       </div>
     );
